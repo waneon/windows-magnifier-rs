15fadc31975c0a5abbe1840c34e9228601bd50df
waneon <waneon.kim@gmail.com>
2021-06-27 21:33:27 +0900

    Magnify more smoothly

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,43 @@ static KEY: u32 = b'1' as u32;
 static MAGNIFYING_FACTOR: f32 = 2.0;
 static TIMER_MS: u32 = 8;
 
+struct MagInfo {
+    mul_x: f32,
+    mul_y: f32,
+    sub_x: i32,
+    sub_y: i32,
+    max_x: i32,
+    max_y: i32,
+}
+
+impl MagInfo {
+    fn init() -> MagInfo {
+        let x: i32;
+        let y: i32;
+
+        unsafe {
+            x = GetSystemMetrics(SM_CXSCREEN);
+            y = GetSystemMetrics(SM_CYSCREEN);
+        }
+
+        let k = (x as f32 / 9.0) as i32;
+        let mul_x = x as f32 * (1.0 - 1.0 / MAGNIFYING_FACTOR) / (x - 2 * k) as f32;
+        let mul_y = y as f32 * (1.0 - 1.0 / MAGNIFYING_FACTOR) / (y - 2 * k) as f32;
+
+        MagInfo {
+            mul_x,
+            mul_y,
+            sub_x: (k as f32 * mul_x) as i32,
+            sub_y: (k as f32 * mul_y) as i32,
+            max_x: ((x as f32) * (1.0 - 1.0 / MAGNIFYING_FACTOR)) as i32,
+            max_y: ((y as f32) * (1.0 - 1.0 / MAGNIFYING_FACTOR)) as i32,
+        }
+    }
+}
+
 fn main() {
     let mut magnified = false;
+    let info = MagInfo::init();
 
     unsafe {
         // Hide console
@@ -46,7 +81,7 @@ fn main() {
             // Turn on magnifying
             if msg.message == WM_TIMER {
                 if magnified {
-                    magnify_on();
+                    magnify_on(&info);
                 }
             }
         }
@@ -59,18 +94,33 @@ fn main() {
     }
 }
 
-fn magnify_on() {
+fn magnify_on(info: &MagInfo) {
     let mut point = POINT::default();
+    let x: i32;
+    let y: i32;
 
     unsafe {
-        // Get cursor point
         GetCursorPos(&mut point);
+        x = (point.x as f32 * info.mul_x) as i32 - info.sub_x;
+        y = (point.y as f32 * info.mul_y) as i32 - info.sub_y;
 
-        MagSetFullscreenTransform(
-            MAGNIFYING_FACTOR,
-            (point.x as f32 * (1.0 / MAGNIFYING_FACTOR)) as i32,
-            (point.y as f32 * (1.0 / MAGNIFYING_FACTOR)) as i32,
-        );
+        // Check bound
+        let x = if x < 0 {
+            0
+        } else if x > info.max_x {
+            info.max_x
+        } else {
+            x
+        };
+        let y = if y < 0 {
+            0
+        } else if y > info.max_y {
+            info.max_y
+        } else {
+            y
+        };
+
+        MagSetFullscreenTransform(MAGNIFYING_FACTOR, x, y);
     }
 }
 
